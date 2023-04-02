9e36b7bfce1287aeb978dd340966c8a26ac1e24b
justynajanczy <justyna5252@gmail.com>
2023-04-02 23:50:20 +0200

    Kamtjatka, Facility, Appointment cat, administrator models

---
+++ BPR.Models/Administrator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPR.Models
+{
+    class Administrator
+    {
+        public int AdministratorId { get; set; }
+        public string AdministratorFirstName { get; set; }
+        public string AdministratorSurname { get; set; }
+        public string AdministratorEmail { get; set; }
+        public string AdministratorPhoneNumber { get; set; }
+    }
+}
---
+++ BPR.Models/AppointmentCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPR.Models
+{
+    class AppointmentCategory
+    {
+        public int AppointmentCategoryId { get; set; }
+        public string AppointmentCategoryName { get; set; }
+        public int AppointmentCategoryDuration { get; set; }
+    }
+}
