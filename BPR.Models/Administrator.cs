94e0a72a5b771d8454ec472452f0bb114ddf284b
xandrusia <sandragrzelak00@wp.pl>
2023-04-04 14:20:22 +0200

    Added attributes and validation

--- BPR.Models/Administrator.cs
+++ BPR.Models/Administrator.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BPR.Models
 {
-    class Administrator
+   public class Administrator
     {
         [Key]
         public int AdministratorId { get; set; }
