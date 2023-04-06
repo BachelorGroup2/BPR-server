52688e1b32f99953a24ad76834968bc3f41759bc
xandrusia <sandragrzelak00@wp.pl>
2023-04-06 23:08:16 +0200

    Added startup

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,8 +1,40 @@
 
+using BPR.API.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace WebApplication1
 {
     public class Startup
     {
-        
+        public Startup(IConfiguration configuration)
+        {
+            configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MyDBContext>(o => o.UseNpgsql("Data source=BPR Kamtjatka"));
+            services.AddControllers();
+             
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+        }
     }
 }
