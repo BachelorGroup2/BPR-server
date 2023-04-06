a2273c5aabccaccfe43073c812384ebe4435431a
justynajanczy <justyna5252@gmail.com>
2023-04-06 20:51:07 +0200

    add rest of config

---
+++ WebApplication1/Models/MyDBContext.cs
@@ -0,0 +1,36 @@
+﻿using BPR.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPR.API.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Administrator> Administrators { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<AppointmentCategory> AppointmentCategories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Facility> Facilities { get; set; }
+        public DbSet<Finance> Finances { get; set; }
+        public DbSet<FinanceCategory> FinanceCategories { get; set; }
+        public DbSet<KamtjatkaInfo> KamtjatkaInfos { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomBooking> RoomBookings { get; set; }
+        public DbSet<RoomCategory> RoomCategories { get; set; }
+        public DbSet<Schedule> Schedules { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
