b9b5def5bcd39b38b8db6f50743c8bc785770f61
xandrusia <sandragrzelak00@wp.pl>
2023-04-06 23:37:05 +0200

    updated startup and installed packages for migrations

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -7,14 +7,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+
 namespace WebApplication1
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
-            configuration = configuration;
+            Configuration = configuration;
         }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
