125015355642b166fdd1bead8d779b140c8c2ad4
justynajanczy <50676200+justynajanczy@users.noreply.github.com>
2023-04-16 21:50:52 +0200

    Delete BPR.API directory

--- BPR.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BPR.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- BPR.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BPR.API.WebApiApplication" Language="C#" %>
