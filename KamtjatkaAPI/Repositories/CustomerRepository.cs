a8ccf6b5b17ba377d0e608828460e1a5430bfc84
xandrusia <sandragrzelak00@wp.pl>
2023-05-04 22:47:31 +0200

    Updated Customer repo

--- KamtjatkaAPI/Repositories/CustomerRepository.cs
+++ KamtjatkaAPI/Repositories/CustomerRepository.cs
@@ -1,4 +1,5 @@
 ﻿using KamtjatkaAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +9,29 @@ namespace KamtjatkaAPI.Repositories
 {
 
         public class CustomerRepository : RepositoryBase<Customer>, ICustomerRepository
-        {
-            public CustomerRepository(vujeeaxiContext repositoryContext)
-              : base(repositoryContext)
+    {
+        private readonly vujeeaxiContext appDbContext;
+        public CustomerRepository(vujeeaxiContext appDbContext)
+              : base(appDbContext)
             {
-
-            }
+            this.appDbContext = appDbContext;
         }
+
+
+        public async Task<IEnumerable<Customer>> GetCustomerWithRoomCategory()
+        {
+            return await appDbContext.Customers.Include(f => f.RoomBookings).ThenInclude(rb => rb.Room.RoomCategory)
+
+            .Select(f => new Customer
+            {
+                Id = f.Id,
+                FirstName = f.FirstName,
+                Surname = f.Surname,
+                Email = f.Email,
+                RoomBookings = f.RoomBookings,
+            })
+            .ToListAsync();
+        }
+    }
 }
 
--- KamtjatkaAPI/Repositories/ICustomerRepository.cs
+++ KamtjatkaAPI/Repositories/ICustomerRepository.cs
@@ -8,5 +8,6 @@ namespace KamtjatkaAPI.Repositories
 {
     public interface ICustomerRepository : IRepositoryBase<Customer>
     {
+        Task<IEnumerable<Customer>> GetCustomerWithRoomCategory();
     }
 }
