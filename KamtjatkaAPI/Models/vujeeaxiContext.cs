ab5852dafd8ebb5a51808673111e7aa23c82904e
justynajanczy <justyna5252@gmail.com>
2023-05-23 16:50:00 +0200

    removed warning

--- KamtjatkaAPI/Models/vujeeaxiContext.cs
+++ KamtjatkaAPI/Models/vujeeaxiContext.cs
@@ -37,7 +37,6 @@ namespace KamtjatkaAPI.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseNpgsql("Server=dumbo.db.elephantsql.com;Database=vujeeaxi;Port=5432;User Id=vujeeaxi;Password=cAZW3QOqk0SeqDKYfIC-sRIW0htmsx7f;");
             }
         }
